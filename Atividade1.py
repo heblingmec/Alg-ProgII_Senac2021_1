1c7ddaf1085500edfe408f04a0a492e3a043a125
Pablo Hebling Guimarães <heblingmec@hotmail.com>
2021-03-18 07:16:03 -0300

    Primeira Versão

---
+++ Atividade1.py
@@ -0,0 +1,27 @@
+'''
+Atividade Aula01
+
+Construir um algoritmo que contenha 3 listas:
+
+    - Nomes de produtos
+    - Preços de cada produto
+    - Quantidades de cada produto
+
+Construir uma função para imprimir um dos produtos da lista
+e uma função para retirar um dos produtos das listas.
+
+Forma de entrega desta atividade: Crie um repositório remoto
+Git no GitHub, GitLab ou Bitbucket e poste aqui apenas o link
+do repositório que contém o código que você desenvolveu.
+'''
+
+lst_nome  = []
+lst_preco = []
+lst_qtd   = []
+
+menu = '''
+
+    1 - Escolha o Produto
+    2 - Remover o Produto
+
+Escolha: '''
